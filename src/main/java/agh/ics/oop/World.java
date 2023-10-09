a67796d330ea4c089f2865b08538a1370541e016
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-10-09 16:13:36 +0200

    lab1 pushed gradle dependencies

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -4,14 +4,14 @@ import agh.ics.oop.model.MoveDirection;
 
 public class World {
     public static void main(String[] arg) {
-        System.out.println("system wystartował");
+        System.out.println("system wystartowal");
         for (String s : arg) {
             System.out.print(s+" ");
         }
         System.out.println();
         MoveDirection[] moves = OptionParser.parse(arg);
         run(moves);
-        System.out.println("system zakończył działanie");
+        System.out.println("system zakonczyl dzialanie");
     }
 
     public static void run(MoveDirection[] moves) {
@@ -19,7 +19,7 @@ public class World {
         for (MoveDirection m : moves) {
             switch (m) {
                 case FORWARD-> System.out.println("do przodu");
-                case BACKWARD-> System.out.println("do tyłu");
+                case BACKWARD-> System.out.println("do tylu");
                 case RIGHT-> System.out.println("w prawo");
                 case LEFT-> System.out.println("w lewo");
             }
