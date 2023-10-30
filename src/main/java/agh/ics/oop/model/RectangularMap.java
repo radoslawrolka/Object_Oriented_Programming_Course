02e3e5b1998092d8e9d02da4a9e4d6b9108f3783
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-10-30 16:26:49 +0100

    lab4 created rectangularmap

---
+++ src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -0,0 +1,56 @@
+package agh.ics.oop.model;
+
+import agh.ics.oop.MapVisualizer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RectangularMap implements WorldMap, MoveValidator {
+    Map<Vector2d, Animal> animals = new HashMap<>();
+    private final Vector2d lowerLeft;
+    private final Vector2d upperRight;
+
+    public RectangularMap(int width, int height) {
+        this.lowerLeft = new Vector2d(0, 0);
+        this.upperRight = new Vector2d(width, height);
+    }
+
+    @Override
+    public boolean canMoveTo(Vector2d position) {
+        return position.follows(lowerLeft) && position.precedes(upperRight) && !isOccupied(position);
+    }
+
+    @Override
+    public boolean place(Animal animal) {
+        if (canMoveTo(animal.getPosition())) {
+            animals.put(animal.getPosition(), animal);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isOccupied(Vector2d position) {
+        return objectAt(position) != null;
+    }
+
+    @Override
+    public Animal objectAt(Vector2d position) {
+        return animals.get(position);
+    }
+
+    @Override
+    public void move(Animal animal, MoveDirection direction) {
+        Vector2d oldPosition = animal.getPosition();
+        animal.move(this, direction);
+        Vector2d newPosition = animal.getPosition();
+        if (oldPosition != newPosition) {
+            animals.remove(oldPosition);
+            animals.put(newPosition, animal);
+        }
+    }
+
+    public String toString() {
+        return new MapVisualizer(this).draw(lowerLeft, upperRight);
+    }
+}
