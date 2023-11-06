edbcd8d008fa4fbdae4c912cf38e65c44ce71b2e
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-11-06 11:34:50 +0100

    lab4 border hotfix

--- src/main/java/agh/ics/oop/model/RectangularMap.java
+++ src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -12,7 +12,7 @@ public class RectangularMap implements WorldMap, MoveValidator {
 
     public RectangularMap(int width, int height) {
         this.lowerLeft = new Vector2d(0, 0);
-        this.upperRight = new Vector2d(width, height);
+        this.upperRight = new Vector2d(width-1, height-1);
     }
 
     @Override
