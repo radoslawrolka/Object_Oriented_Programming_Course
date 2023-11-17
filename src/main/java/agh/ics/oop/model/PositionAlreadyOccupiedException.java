ee2e6d9e4166412ed524da636e607f0b355753fd
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-11-17 14:27:03 +0100

    lab6 refractor

--- src/main/java/agh/ics/oop/model/PositionAlreadyOccupiedException.java
+++ src/main/java/agh/ics/oop/model/PositionAlreadyOccupiedException.java
@@ -2,7 +2,7 @@ package agh.ics.oop.model;
 
 public class PositionAlreadyOccupiedException extends Exception {
     public PositionAlreadyOccupiedException(Vector2d position) {
-        super("Position (" + position.getX() + ", " + position.getY() + ") is already occupied.");
+        super("Position "+ position.toString() +" is already occupied.");
     }
 }
 
