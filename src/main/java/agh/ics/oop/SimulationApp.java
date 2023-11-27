0e195e6108e6124abc193cc80fd053620671904f
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-11-27 15:14:10 +0100

    lab8 gui app start

---
+++ src/main/java/agh/ics/oop/SimulationApp.java
@@ -0,0 +1,10 @@
+package agh.ics.oop;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SimulationApp extends Application {
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
