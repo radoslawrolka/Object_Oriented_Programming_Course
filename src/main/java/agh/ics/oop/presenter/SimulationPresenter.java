76eca74ef7988e436f217be19395816671644d39
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-11-29 14:24:30 +0100

    lab7 working gui, centred

--- src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -4,9 +4,13 @@ import agh.ics.oop.Simulation;
 import agh.ics.oop.model.*;
 import agh.ics.oop.model.util.MapVisualizer;
 import javafx.application.Platform;
+import javafx.geometry.HPos;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.util.List;
 
@@ -14,28 +18,98 @@ import java.util.List;
 public class SimulationPresenter implements MapChangeListener {
     private WorldMap<WorldElement, Vector2d> map;
     @FXML
-    private Label infoLabel;
+    private GridPane mapGrid;
     @FXML
     private TextField moveListTextField;
     @FXML
     private Label moveDescriptionLabel;
 
+    private int xMin;
+    private int yMin;
+    private int xMax;
+    private int yMax;
+    private  int mapWidth;
+    private  int mapHeight;
+
+    private final int width = 50;
+    private final int height = 50;
+
     public void setWorldMap(WorldMap<WorldElement, Vector2d> map) {
         this.map = map;
     }
 
-    public void drawMap(WorldMap map) {
-        infoLabel.setText(map.toString());
+    public void xyLabel(){
+        GridPane.setHalignment(new Label("y/x"), HPos.CENTER);
+        mapGrid.getColumnConstraints().add(new ColumnConstraints(width));
+        mapGrid.getRowConstraints().add(new RowConstraints(height));
+        mapGrid.add(new Label("y/x"), 0, 0);
+    }
+
+    public void updateBounds(){
+        xMin = map.getCurrentBounds().lowerLeft().getX();
+        yMin = map.getCurrentBounds().lowerLeft().getY();
+        xMax = map.getCurrentBounds().upperRight().getX();
+        yMax = map.getCurrentBounds().upperRight().getY();
+        mapWidth = xMax - xMin + 1;
+        mapHeight = yMax - yMin + 1;
+    }
+
+    public void columnsFunction(){
+        for (int i = 0; i < mapWidth; i++){
+            Label label = new Label(Integer.toString(xMin+i));
+            GridPane.setHalignment(label, HPos.CENTER);
+            mapGrid.getColumnConstraints().add(new ColumnConstraints(width));
+            mapGrid.add(label, i+1, 0);
+        }
+    }
+
+    public void rowsFunction(){
+        for (int i = 0; i < mapHeight; i++){
+            Label label = new Label(Integer.toString(yMax-i));
+            GridPane.setHalignment(label, HPos.CENTER);
+            mapGrid.getRowConstraints().add(new RowConstraints(height));
+            mapGrid.add(label, 0, i+1);
+        }
+    }
+
+    public void addElements() {
+        for (int i = xMin; i <= xMax; i++) {
+            for (int j = yMax; j >= yMin; j--) {
+                Vector2d pos = new Vector2d(i,j);
+                if(map.isOccupied(pos)){
+                    mapGrid.add(new Label(map.objectAt(pos).toString()),i-xMin+1,yMax-j+1);
+                }
+                else {
+                    mapGrid.add(new Label(" "),i-xMin+1,yMax-j+1);
+                }
+                mapGrid.setHalignment(mapGrid.getChildren().get(mapGrid.getChildren().size()-1), HPos.CENTER);
+            }
+        }
+    }
+
+    private void drawMap() {
+        updateBounds();
+        xyLabel();
+        columnsFunction();
+        rowsFunction();
+        addElements();
+        mapGrid.setGridLinesVisible(true);
     }
 
     @Override
     public void mapChanged(WorldMap map, String message) {
         setWorldMap(map);
         Platform.runLater(() -> {
-            drawMap(map);
+            clearGrid();
+            drawMap();
             moveDescriptionLabel.setText(message);
         });
+    }
 
+    private void clearGrid() {
+        mapGrid.getChildren().retainAll(mapGrid.getChildren().get(0)); // hack to retain visible grid lines
+        mapGrid.getColumnConstraints().clear();
+        mapGrid.getRowConstraints().clear();
     }
 
     @FXML
